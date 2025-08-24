0c0a320faf9c4dd5fc26ec7b4e6e641592f1b175
Leo Bi <biminglei@gmail.com>
2025-08-24 21:32:10 +0800

    add posting window

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -2,19 +2,28 @@ import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
+import Modal from "./Modal";
+
 
 function PostsList() {
 
     const [bodyText, setBodyText] = useState('');
     const [author, setAuthor] = useState('');
+    const [isModalOpen, setIsModalOpen] = useState(true);
 
     return (
         <>
-            <NewPost onBodyChange={(event) => {
-                setBodyText(event.target.value);
-            }} onAuthorChange={(event) => {
-                setAuthor(event.target.value);
-            }} />
+            { isModalOpen && 
+                <Modal onCloseWindow={() => {
+                    setIsModalOpen(false);
+                }}>
+                    <NewPost onBodyChange={(event) => {
+                        setBodyText(event.target.value);
+                    }} onAuthorChange={(event) => {
+                        setAuthor(event.target.value);
+                    }} />
+                </Modal>
+            }
 
             <ul className={classes.posts}>
                 <Post author={author} body={bodyText}/>
