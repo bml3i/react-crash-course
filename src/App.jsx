035f2d92cbcce5ebfc0b7a72da3ee9b479bd4ba7
Leo Bi <biminglei@gmail.com>
2025-08-24 13:01:40 +0800

    update

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Post from "./components/Post";
+
+function App() {
+  return (
+    <>
+      <Post author="Felix" body="React is awesome" />
+      <Post author="Apple" body="Next.js is great" />
+    </>
+  );
+}
+
+export default App
