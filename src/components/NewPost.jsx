8f868aa0600eec88214d547a70a65df294c32090
Leo Bi <biminglei@gmail.com>
2025-08-25 19:59:16 +0800

    show dynamic posts

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,16 +1,38 @@
 import classes from './NewPost.module.css';
 
+import { useState } from 'react';
+
 function NewPost(props) {
 
+  const [bodyText, setBodyText] = useState('');
+  const [author, setAuthor] = useState('');
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const postData = {
+      body: bodyText,
+      author: author
+    };
+
+    //console.log(postData);
+    props.onAddPost(postData);
+    props.onCancel();
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitFormHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={props.onBodyChange} />
+        <textarea id="body" required rows={3} onChange={(event) => {
+            setBodyText(event.target.value);
+        }} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={props.onAuthorChange} />
+        <input type="text" id="name" required onChange={(event) => {
+            setAuthor(event.target.value);
+        }} />
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={props.onCancel}>Cancel</button>
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,31 +1,42 @@
-import { useState } from "react";
+
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import Modal from "./Modal";
+import { useState } from "react";
 
 
-function PostsList({isPosting, onStopPosting}) {
+function PostsList({ isPosting, onStopPosting }) {
 
-    const [bodyText, setBodyText] = useState('');
-    const [author, setAuthor] = useState('');
+    const [posts, setPosts] = useState([]);
+
+    function addPostHandler(postData) {
+        setPosts((prevPosts) => [postData, ...prevPosts]);
+    }
 
     return (
         <>
-            { isPosting && 
+            {isPosting &&
                 <Modal onCloseWindow={onStopPosting}>
-                    <NewPost onBodyChange={(event) => {
-                        setBodyText(event.target.value);
-                    }} onAuthorChange={(event) => {
-                        setAuthor(event.target.value);
-                    }} onCancel={onStopPosting} />
+                    <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
                 </Modal>
             }
 
-            <ul className={classes.posts}>
-                <Post author={author} body={bodyText}/>
-                <Post author="Apple" body="Next.js is great" />
-            </ul>
+            {posts.length > 0 && (
+                <ul className={classes.posts}>
+                    {posts.map((post) => (
+                        <Post key={post.body} author={post.author} body={post.body} />
+                    ))}
+                </ul>
+            )}
+
+            { posts.length === 0 && (
+                <div style={{textAlign: 'center', color: 'gray'}}>
+                    <h2>There are no posts yet...</h2>
+                    <p>Start adding some!</p>
+                </div>
+            )}
+
         </>
     );
 }
