92753a26e47492914a5cf6dd01680bb8e1b5e666
Leo Bi <biminglei@gmail.com>
2025-08-26 19:37:02 +0800

    fetch posts via API, useEffect  and Loading status.

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -3,14 +3,34 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import Modal from "./Modal";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 function PostsList({ isPosting, onStopPosting }) {
 
     const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
+
+    useEffect(() => {
+        async function fetchPosts() {
+            setIsFetching(true);
+            const response = await fetch('http://localhost:8080/posts');
+            const data = await response.json();
+            setPosts(data.posts);
+            setIsFetching(false);
+        }
+
+        fetchPosts();
+    }, []);
 
     function addPostHandler(postData) {
+        fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
         setPosts((prevPosts) => [postData, ...prevPosts]);
     }
 
@@ -22,7 +42,7 @@ function PostsList({ isPosting, onStopPosting }) {
                 </Modal>
             }
 
-            {posts.length > 0 && (
+            {!isFetching && posts.length > 0 && (
                 <ul className={classes.posts}>
                     {posts.map((post) => (
                         <Post key={post.body} author={post.author} body={post.body} />
@@ -30,12 +50,14 @@ function PostsList({ isPosting, onStopPosting }) {
                 </ul>
             )}
 
-            { posts.length === 0 && (
-                <div style={{textAlign: 'center', color: 'gray'}}>
+            {!isFetching && posts.length === 0 && (
+                <div style={{ textAlign: 'center', color: 'gray' }}>
                     <h2>There are no posts yet...</h2>
                     <p>Start adding some!</p>
                 </div>
             )}
+
+            {isFetching && <div style={{ textAlign: 'center', color: 'gray' }}><p>Loading posts...</p></div>}
 
         </>
     );
