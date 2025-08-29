ba2a2169219c92af6d255b78e5027e702731eeb5
Leo Bi <biminglei@gmail.com>
2025-08-29 09:16:07 +0800

    dynamic routes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,15 @@
 import classes from './Post.module.css'
 
+import { Link } from 'react-router-dom';
+
 function Post(props) {
 
     return (
         <div className={classes.post}>
-            <p className={classes.author}>{props.author}</p>
-            <p className={classes.text}>{props.body}</p>
+            <Link to={'/post/' + props.id}>
+                <p className={classes.author}>{props.author}</p>
+                <p className={classes.text}>{props.body}</p>
+            </Link>
         </div>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import RootLayout from './routes/RootLayout.jsx'
 import './index.css'
 import Posts, {loader as postsLoader} from './routes/Posts.jsx'
 import NewPost, { action as newPostAction } from './routes/NewPost.jsx'
+import PostDetails, {loader as postDetailsLoader} from './routes/PostDetails.jsx'
 
 const router = createBrowserRouter([
   { path: '/', element: <RootLayout/>, children: [
@@ -12,7 +13,10 @@ const router = createBrowserRouter([
       path: '/', 
       element: <Posts />, 
       loader: postsLoader, 
-      children: [{ path: '/create-post', element: <NewPost />, action: newPostAction }]
+      children: [
+        { path: '/create-post', element: <NewPost />, action: newPostAction },
+        { path: '/post/:id', element: <PostDetails />, loader: postDetailsLoader }
+      ]
     }
   ] },
 ]);
